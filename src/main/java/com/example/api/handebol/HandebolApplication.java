58dcbc321667540be0b968a27523d841b054ae21
ronan-ferreira <ronanifto@gmail.com>
2022-11-21 16:58:04 -0300

    spring boot

---
+++ src/main/java/com/example/api/handebol/HandebolApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.handebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandebolApplication.class, args);
+	}
+
+}
